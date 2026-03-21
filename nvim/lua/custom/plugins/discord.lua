c9cad24fd6ca585e7357987c32975551011f9501
sean <91675054+seanhalberthal@users.noreply.github.com>
2026-03-21 22:38:25 +0000

    docs: use tag-based shield for release badge

--- nvim/lua/custom/plugins/discord.lua
+++ nvim/lua/custom/plugins/discord.lua
@@ -1,25 +1,27 @@
-return {
-  {
-    'vyfor/cord.nvim',
-    opts = {
-      display = {
-        theme = 'catppuccin',
-      },
-      idle = {
-        enabled = true,
-        timeout = 600000, -- 10 minutes
-        show_status = false,
-      },
-      buttons = {
-        {
-          label = function(opts)
-            return opts.repo_url and 'View Repository' or 'GitHub'
-          end,
-          url = function(opts)
-            return opts.repo_url
-          end,
-        },
-      },
-    },
-  },
-}
+return {}
+
+-- return {
+--   {
+--     'vyfor/cord.nvim',
+--     opts = {
+--       display = {
+--         theme = 'catppuccin',
+--       },
+--       idle = {
+--         enabled = true,
+--         timeout = 600000, -- 10 minutes
+--         show_status = false,
+--       },
+--       buttons = {
+--         {
+--           label = function(opts)
+--             return opts.repo_url and 'View Repository' or 'GitHub'
+--           end,
+--           url = function(opts)
+--             return opts.repo_url
+--           end,
+--         },
+--       },
+--     },
+--   },
+-- }
