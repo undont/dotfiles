7efdcd1e93d4c947c7a9eee776d355e7d61e0ef0
sean <91675054+undont@users.noreply.github.com>
2026-09-10 11:26:05 +0100

    update: dashboard find text moves to G

--- nvim/lua/custom/plugins/dashboard.lua
+++ nvim/lua/custom/plugins/dashboard.lua
@@ -105,7 +105,7 @@ return {
           keys = {
             { icon = '', key = 'f', desc = 'Find File',    action = ":lua Snacks.dashboard.pick('files')" },
             { icon = '', key = 'n', desc = 'New File',     action = ':ene | startinsert' },
-            { icon = '󰺯', key = 'g', desc = 'Find Text',    action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = '󰺯', key = 'G', desc = 'Find Text',    action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = '', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = '', key = 'p', desc = 'PRs',          action = ":Differ pr list" },
             { icon = '', key = 'c', desc = 'Config',       action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
